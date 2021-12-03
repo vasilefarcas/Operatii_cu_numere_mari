3df1ad1f2efb7fd28d4ca1d968c8ca231e053bcc
vasilefarcas <farcasvasile06@gmail.com>
2021-12-03 14:35:53 +0200

    Adaugare_clasa_adunare

---
+++ Operatii_cu_numere_mari/Operatii_cu_numere_mari/Adunare.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatii_cu_numere_mari
+{
+    class Adunare
+    {
+        /// <summary>
+        /// Aceasta functie aduna doua numere de dimensiuni mari introduse de la tastatura.
+        /// </summary>
+        public static void Adunare_Numere()
+        {
+            Console.Clear();
+            Console.WriteLine("Introduceti primul numar:");
+            string primul, al_doilea;
+            primul = Console.ReadLine();
+            Console.WriteLine("Introduceti cel de al doilea numar:");
+            al_doilea = Console.ReadLine();
+            // lungimea celui mai mare dintre cele doua numere si lungimea celui mai mic dintre cele doua numere.
+            int max = Math.Max(primul.Length, al_doilea.Length), min = Math.Min(primul.Length, al_doilea.Length);
+            int[] rezultat = new int[max];
+            int i = max - 1;
+            // aici vom stoca pe rand valoarea celor doua numere din cele doua siruri.
+            int unu, doi, ok = 0;
+            for (; i >= 0 || i >= min; i--)
+            {
+                unu = Convert.ToInt32(primul[i]);
+                doi = Convert.ToInt32(al_doilea[i]);
+                rezultat[i] = (unu + doi) % 10 + ok;
+                ok = 0;
+                if (unu + doi > 9)
+                    ok = 1;
+                else
+                    ok = 0;
+            }
+            int l1 = primul.Length, l2 = al_doilea.Length;
+            if (l1 != l2)
+            {
+                if (l1 > l2)
+                    for (i = 0; i < min; i++)
+                        rezultat[i] = primul[i];
+                if (l2 > l1)
+                    for (i = 0; i < min; i++)
+                        rezultat[i] = primul[i];
+            }
+            for (i = 0; i < max; i++)
+                Console.Write(rezultat[i]);
+
+        }
+
+    }
+}
--- Operatii_cu_numere_mari/Operatii_cu_numere_mari/Program.cs
+++ Operatii_cu_numere_mari/Operatii_cu_numere_mari/Program.cs
@@ -10,6 +10,41 @@ namespace Operatii_cu_numere_mari
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Operatiile care se pot executa sunt: adunare, scadere, inmultire, impartire, ridicare la putere si radacina patrata.\n" +
+                "Fiecare operatie are un numar. Pentru a efectua operatia respectiva introduceti numarul si apasati tasta \"ENTER\"\n" +
+                "1 - adunare\n" +
+                "2 - scadere\n" +
+                "3 - inmultire\n" +
+                "4 - impartire\n" +
+                "5 - ridicare la putere\n" +
+                "6 - radacina patrata\n" +
+                "Introduceti varianta:");
+            int varianta = 0;
+            while (varianta > 6 || varianta < 1)
+                try
+                {
+                    varianta = int.Parse(Console.ReadLine());
+                    while (varianta > 6 || varianta < 1)
+                    {
+                        Console.WriteLine(varianta);
+                        Console.WriteLine("Introduceti o valoare dintre cele valide:");
+                    }
+                }
+                catch (System.FormatException)
+                {
+
+                    Console.WriteLine("Introduceti doar valori numerice din intervalul [1,6].");
+                }
+            switch (varianta)
+            {
+                case 1: { Adunare.Adunare_Numere();
+                        break;
+                    }
+                default: 
+                    Console.WriteLine(" ");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
