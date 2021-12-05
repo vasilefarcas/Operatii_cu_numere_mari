7aaaa7389096a30b2c9f6ca383b177cce646f6a4
vasilefarcas <farcasvasile06@gmail.com>
2021-12-05 17:30:23 +0200

    ProgramFinalAdunare+AdaugareClasaScadere
    Am terminat clasa in care realizez adunare si am adaugat noua clasa in care voi realiza scaderea

--- Operatii_cu_numere_mari/Operatii_cu_numere_mari/Adunare.cs
+++ Operatii_cu_numere_mari/Operatii_cu_numere_mari/Adunare.cs
@@ -8,24 +8,49 @@ namespace Operatii_cu_numere_mari
 {
     class Adunare
     {
-
         /// <summary>
-        /// In aceasta metoda o sa 
+        /// Metoda in care punem laolalta toate metodele create pentru a aduna cele doua numere de dimensiuni mari.
         /// </summary>
         public static void Adunare_Numere()
         {
+            // Declaram doua variabile de tip string in care vom salva numerele
             string primul = string.Empty, al_doilea = string.Empty;
+            // Introducem numerele de la tastatura, iar transmiterea citirii
+            // in program se face prin referinta
             Citire_Numere(ref primul, ref al_doilea);
+            // Declar vectorii in care voi stoca numerele pe rand
             int[] v = new int[primul.Length], a = new int[al_doilea.Length];
+            // Convertesc sirurile de caractere in tip int
             Convertire(ref v, primul);
             Convertire(ref a, al_doilea);
+            // Vom aborda diferit adunarea in cazul in care sirurile de numere au 
+            // dimensiuni diferite
             if (primul.Length == al_doilea.Length)
                 Afisare_Rezultat(Adunare_Egale(v, a));
             else
-                Afisare_Rezultat(Adunare_Inegale3(v, a));
+                Afisare_Rezultat(Adunare_Inegale(v, a));
 
         }
 
+        /// <summary>
+        /// Metoda prin care citim cele doua numere in variabile de tip string.
+        /// </summary>
+        /// <param name="primul">Primul sir de caractere.</param>
+        /// <param name="al_doilea">Al doilea sir de caractere.</param>
+        private static void Citire_Numere(ref string primul, ref string al_doilea)
+        {
+            Console.Clear();
+            Console.WriteLine("Introduceti primul numar:");
+            primul = Console.ReadLine();
+            Console.WriteLine("Introduceti cel de al doilea numar:");
+            al_doilea = Console.ReadLine();
+        }
+        
+        /// <summary>
+        /// Metoda care converteste un sir de caractere in vector de tipul int. 
+        /// </summary>
+        /// <param name="a">Vectorul in care dorim stocam numarul.</param>
+        /// <param name="b">Variabila de tip string pe care o vom converti in vector de tip int.</param>
         private static void Convertire(ref int[] a, string b)
         {
             int i = 0;
@@ -35,20 +60,18 @@ namespace Operatii_cu_numere_mari
                 i++;
             }
         }
-
-        private static void Citire_Numere(ref string primul, ref string al_doilea)
-        {
-            Console.Clear();
-            Console.WriteLine("Introduceti primul numar:");
-            primul = Console.ReadLine();
-            Console.WriteLine("Introduceti cel de al doilea numar:");
-            al_doilea = Console.ReadLine();
-        }
-
+        
+        /// <summary>
+        /// Metoda care aduna doua numere de lungimi egale si returneaza printr-un vector rezultatul. 
+        /// </summary>
+        /// <param name="a">Primul numar sub forma de vector.</param>
+        /// <param name="b">Al doilea numar sub forma de vector.</param>
+        /// <returns></returns>
         private static int[] Adunare_Egale(int[] a, int[] b)
         {
-            int l = a.Length, i, j;
-            // Adunarea
+            // l reprezinta lungimea numerelor.
+            int l = a.Length, i;
+            // Vectorul c in care vom stoca rezultatul.
             int[] c = new int[l + 2];
 
             for (i = 0; i < l; i++)
@@ -57,175 +80,55 @@ namespace Operatii_cu_numere_mari
                 if (a[i] + b[i] > 9)
                     c[i + 1]++;
             }
+            // Inversam vectorul c pentru ca rezultatul sa fie corect afisat.
             Array.Reverse(c);
+            // Returnam rezultatul.
             return c;
         }
-
-        private static void Adunarea(string primul, string al_doilea)
-        {
-            int max = Math.Max(primul.Length, al_doilea.Length), min = Math.Min(primul.Length, al_doilea.Length);
-            if (max == min)
-            {
-                int[] rezultat = new int[max];
-                int i = max - 1;
-                int unu, doi, ok = 0;
-                for (; i >= 0; i--)
-                {
-                    // aici vom stoca pe rand valoarea celor doua numere din cele doua siruri.
-                    unu = int.Parse(Convert.ToString(primul[i]));
-                    doi = int.Parse(Convert.ToString(primul[i]));
-                    rezultat[i] = (unu + doi) % 10 + ok;
-                    ok = 0;
-                    // in variabila ok stocam valoarea 1 in cazul in care suma celor doua numere este mai mare decat 9
-                    if (unu + doi > 9)
-                        ok = 1;
-                    else
-                        ok = 0;
-                }
-                if (int.Parse(Convert.ToString(primul[0])) + int.Parse(Convert.ToString(primul[0])) > 9)
-                    ok = 1;
-                else
-                    ok = 0;
-                // Afisare_Rezultat(rezultat, ok);
-            }
-        }
-
-        private static void Afisare_Vector(int[] v)
-        {
-            for (int i = 0; i < v.Length; i++)
-                Console.Write(v[i]);
-        }
-
+        
+        /// <summary>
+        /// Metoda care afiseaza rezultatul.
+        /// </summary>
+        /// <param name="v">Vectorul pe care il vom afisa ca rezultat.</param>
         private static void Afisare_Rezultat(int[] v)
         {
             Console.WriteLine("Rezultatul este:");
             int i = 0;
+            // Sarim peste valorile de 0 de la inceputul sirului in cazul in care acestea exista.
             while (v[i] == 0)
                 i++;
+            // Afisam vectorul.
             for (; i < v.Length; i++)
                 Console.Write(v[i]);
         }
+
         /// <summary>
-        /// Aceasta functie aduna doua numere de dimensiuni mari introduse de la tastatura.
+        /// Metoda care aduna doua numere de lungimi diferite si returneaza printr-un vector rezultatul.
         /// </summary>
-        public static void Adunare_Numere1()
-        {
-            Console.Clear();
-            Console.WriteLine("Introduceti primul numar:");
-            string primul, al_doilea;
-            primul = Console.ReadLine();
-            Console.WriteLine("Introduceti cel de al doilea numar:");
-            al_doilea = Console.ReadLine();
-            // lungimea celui mai mare dintre cele doua numere si lungimea celui mai mic dintre cele doua numere.
-            int max = Math.Max(primul.Length, al_doilea.Length), min = Math.Min(primul.Length, al_doilea.Length);
-            if (min == max)
-            {
-                int[] rezultat = new int[max];
-                int i = max - 1;
-                int unu, doi, ok = 0;
-                for (; i >= 0 || i > min; i--)
-                {
-                    // aici vom stoca pe rand valoarea celor doua numere din cele doua siruri.
-                    unu = int.Parse(Convert.ToString(primul[i]));
-                    doi = int.Parse(Convert.ToString(al_doilea[i]));
-                    rezultat[i] = (unu + doi) % 10 + ok;
-                    ok = 0;
-                    // in variabila ok stocam valoarea 1 in cazul in care suma celor doua numere este mai mare decat 9
-                    if (unu + doi > 9)
-                        ok = 1;
-                    else
-                        ok = 0;
-                }
-
-                // afisarea rezultatului 
-                for (i = 0; i < max; i++)
-                    Console.Write(rezultat[i]);
-
-            }
-
-        }
-
-
+        /// <param name="a">Primul numar sub forma de vector</param>
+        /// <param name="b">Al doilea numar sub forma de vector</param>
+        /// <returns></returns>
         private static int[] Adunare_Inegale(int[] a, int[] b)
         {
-            int l1 = a.Length, l2 = b.Length, i, j, ok = 0, lun = Math.Min(l1, l2);
-            // Adunarea
-            int[] c = new int[Math.Max(l1, l2) + 2];
+            // l1 respectiv l2 stocheaza lungimile vectorilor de numere.
+            // max stocheaza lungimea maxima dintre cele doua siruri.
 
-            for (i = 0; i < lun; i++)
-            {
-                c[i] = c[i] + (a[i] + b[i]) % 10;
-                if (a[i] + b[i] > 9)
-                    c[i + 1]++;
-            }
-
-            if (lun == l1)
-                while (i < Math.Max(l1, l2))
-                {
-                    c[i] = (b[i] + ok) % 10;
-                    i++;
-                }
-            else
-                if (lun == l2)
-                while (i < Math.Max(l1, l2))
-                {
-                    c[i] = (a[i] + ok) % 10;
-                    i++;
-                }
-            return c;
-        }
-
-        private static int[] Adunare_Inegale2(int[] a, int[] b)
-        {
-            int l1 = a.Length, l2 = b.Length, i, j;
-            int[] c = new int[Math.Max(l1, l2) + 2];
-            i = 0;
-            if (l1 < l2)
-            {
-                while (l1 + i < l2)
-                {
-                    c[i] = b[i];
-                    i++;
-                }
-                int q;
-                j = Math.Max(l1, l2);
-                for (q = 0; i < j || q < l1; i++, q++)
-                {
-                    c[i] = c[i] + (a[q] + b[i]) % 10;
-                    if (a[q] + b[i] > 9)
-                        c[i + 1]++;
-                }
-
-            }
-            if (l2 < l1)
-            {
-                while (l2 + i < l1)
-                {
-                    c[i] = a[i];
-                    i++;
-                }
-                int q;
-                j = Math.Max(l1, l2);
-                for (q = 0; i < j || q < l2; i++, q++)
-                {
-                    c[i] = c[i] + (a[i] + b[q]) % 10;
-                    if (a[i] + b[q] > 9)
-                        c[i + 1]++;
-                }
-            }
-            return c;
-        }
-
-        private static int[] Adunare_Inegale3(int[] a, int[] b)
-        {
             int l1 = a.Length, l2 = b.Length, i, j, max = Math.Max(l1, l2) + 1;
+            // Declararea noului vector care reprezinta rezultatul final al adunarii, 
+            // Acesta are lungimea maximului dintre cele doua plus doua pozitii in cazul
+            // in care rezultatul o sa necesite asta
             int[] c = new int[Math.Max(l1, l2) + 2];
+            // Pornim cu fiecare vector de la final si adaugam in noul vector creat de la final.
             for (i = l1 - 1, j = l2 - 1; i >= 0 && j >= 0; i--, j--, max--)
             {
                 c[max] = c[max] + (a[i] + b[j]) % 10;
+                // In cazul in care suma celor doua numere de pe pozitia i si j este mai mare decat 9
+                // vom adauga 1 la pozitia urmatoare.
                 if (a[i] + b[j] > 9)
                     c[max - 1]++;
             }
+            // Aici completam din numarul de lungime mai mare partea care nu a fost adaugata in 
+            // for-ul anterior
             while (i >= 0)
             {
                 c[max] = c[max] + a[i];
@@ -241,6 +144,15 @@ namespace Operatii_cu_numere_mari
             return c;
         }
 
+        /// <summary>
+        /// Metoda care afiseaza un vector de tip int.
+        /// </summary>
+        /// <param name="v">Vectorul pe care il vom afisa</param>
+        private static void Afisare_Vector(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+                Console.Write(v[i]);
+        }
 
     }
 }
